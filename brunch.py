a026ab9a3e155cc9feff5afab614c41651d8b4aa
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2023-09-29 16:51:56 -0400

    Update brunch.py

--- brunch.py
+++ brunch.py
@@ -1,2 +1,12 @@
 def brunch(input):
-    
+    cost = input[0]
+    people = input[-1]
+    y1 = people*input[1]
+    y2 = people*input[2]
+    y3 = people*input[3]
+    y4 = people*input[4]
+    funding = y1 * 12 + y2 * 10 + y3 * 7 + y4 * 5
+    if(funding/2 < cost):
+        return "YES"
+    else:
+        return "NO"
