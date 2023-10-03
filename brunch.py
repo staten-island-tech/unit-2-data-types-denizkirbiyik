ca5c80a52921c186828a7a12fe1265d9172cc12b
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2023-10-03 14:01:50 -0400

    Update brunch.py

--- brunch.py
+++ brunch.py
@@ -1,12 +1,21 @@
 def brunch(input):
     cost = input[0]
     people = input[-1]
-    extra = 0
-    y1 = people*input[1]
-    y2 = people*input[2]
-    y3 = people*input[3]
-    y4 = people*input[4]
-    extra += y1 % 1 + y1 % 1 + y1 % 1
+    y1 = people*input[1] - (people*input[1] % 1)
+    y2 = people*input[2] - (people*input[2] % 1)
+    y3 = people*input[3] - (people*input[3] % 1)
+    y4 = people*input[4] - (people*input[4] % 1)
+    extra = (people*input[1] % 1) + (people*input[2] % 1) + (people*input[3] % 1) + (people*input[4] % 1)
+    print(extra)
+    m = max([(people*input[1] % 1), (people*input[2] % 1), (people*input[3] % 1), (people*input[4] % 1)])
+    if(m == (people*input[1] % 1)):
+        y1 += extra
+    elif(m == (people*input[2] % 1)): 
+        y2 += extra
+    elif(m == (people*input[3] % 1)): 
+        y3 += extra
+    elif(m == (people*input[4] % 1)): 
+        y4 += extra
     funding = y1 * 12 + y2 * 10 + y3 * 7 + y4 * 5
     if(funding/2 < cost):
         return "YES"
@@ -15,5 +24,6 @@ def brunch(input):
 def final(input):
     f = []
     for i in range(10):
-        f.append(brunch(input[0][6*i], input[0][6*i+1], input[0][6*i+2], input[0][6*i+3], input[0][6*i+4], input[0][6*i+5]))
+        f.append(brunch(input[6*i], input[6*i+1], input[6*i+2], input[6*i+3], input[6*i+4], input[6*i+5]))
     return f
+print(brunch([5727, 0.1, 0.15, 0.4, 0.35, 477]))
