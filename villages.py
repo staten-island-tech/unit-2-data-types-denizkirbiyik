c49799a80e289108c88bb86cc9d2fe350ea5e308
Deniz Kirbiyik <58996968+denizkirbiyik@users.noreply.github.com>
2023-10-04 18:03:00 -0400

    Update villages.py

--- villages.py
+++ villages.py
@@ -1,15 +1,19 @@
-def insert(input):
-    for i in range(1, len(input)):
-        cur = input[i]
+def insert(inp):
+    for i in range(1, len(inp)):
+        cur = inp[i]
         j = i - 1    
-        while j >= 0 and cur < input[j]:
-            input[j + 1] = input[j]
+        while j >= 0 and cur < inp[j]:
+            inp[j + 1] = inp[j]
             j -= 1
-        input[j + 1] = cur
-    return input
+        inp[j + 1] = cur
+    return inp
 
-def village(input):
-    N = input[0]
-    input.remove(N)
-    input = insert(input)
-
+def village(inp):
+    N = inp[0]
+    inp.remove(N)
+    inp = insert(inp)
+    distances = []
+    for i in range(N-2):
+        distances.append(inp[i+2]/2-inp[i]/2)
+    distances = insert(distances)
+    return distances[0]
